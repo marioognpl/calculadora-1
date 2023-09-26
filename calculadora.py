a95eee99867d30736031f3670e4654b69071b4b5
Emilio <zx22student3204@colegiovalledelmiro.es>
2023-09-26 10:19:21 +0200

    Creado funcion sumar

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+#!C:\Users\zx22student3204\AppData\Local\Programs\Python\Python311\python.exe
+
+print("Content-type: text/html\n")
+
+
+#Metodo sumar
+
+def suma():
+    result = 5 + 3
+    return result
+
+print(suma())
